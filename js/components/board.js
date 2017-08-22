e31623e0f1f18da3d2a6ccb4ab7f4234a7d50acc
LarissaMorrell <larissa@larissamorrell.com>
2017-08-22 15:46:38 -0400

    init

---
+++ js/components/board.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import List from './list';
+
+export default function Board() {
+  var lists = [];
+  for(let i=0; i < 3; i++){
+    lists.push(<List />);
+  }
+
+  return (
+    <div className="board">
+      {lists}
+    </div>
+  );
+}
